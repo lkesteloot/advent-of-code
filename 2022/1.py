efddb63e01019fb1e5bfe946c3a34fe2955b615f
Lawrence Kesteloot <lk@teamten.com>
2022-11-30 21:52:46 -0800

    More terse day 1.

--- 2022/1.py
+++ 2022/1.py
@@ -1,26 +1,10 @@
 
-# mv ~/Downloads/input.txt input-xx.txt
-
-import sys
-from collections import defaultdict, Counter
-from itertools import *
-from functools import *
 from more_itertools import *
-# import numpy as np
-# import scipy.signal
 
 lines = [line.strip() for line in open("input-1.txt")]
-# matrix = [list(map(int, list(line))) for line in lines]
 
-cals = []
-total = 0
-for line in lines:
-    if line == "":
-        cals.append(total)
-        total = 0
-    else:
-        total += int(line)
-
-cals.sort()
+cals = split_at(lines, lambda line: line == "")
+cals = sorted(sum(int(cal) for cal in elf) for elf in cals)
+print(cals[-1])
 print(sum(cals[-3:]))
 
