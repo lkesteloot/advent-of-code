96686ad4e04b3864294024f13ef87ac76870c358
Lawrence Kesteloot <lk@teamten.com>
2022-12-11 21:52:08 -0800

    Day 12

--- 2022/10.py
+++ 2022/10.py
@@ -1,19 +1,8 @@
 
-
-# mv ~/Downloads/input.txt input-10.txt
-
 import sys
-from collections import defaultdict, Counter
-from itertools import *
-from functools import *
-# https://more-itertools.readthedocs.io/en/stable/
-from more_itertools import *
-# import numpy as np
-# import scipy.signal
 
 lines = [line.strip() for line in open("input-10-test.txt")]
 lines = [line.strip() for line in open("input-10.txt")]
-# matrix = [list(map(int, list(line))) for line in lines]
 
 W = 40
 H = 6
@@ -27,9 +16,8 @@ col = 0
 def check():
     global cycle, x, total, col, row
 
-    if False and cycle in [20, 60, 100, 140, 180, 220]:
+    if cycle in [20, 60, 100, 140, 180, 220]:
         v = cycle*x
-        print(cycle, x, v)
         total += v
 
     if abs(col - x) <= 1:
---
+++ 2022/12.py
@@ -0,0 +1,76 @@
+
+
+# mv ~/Downloads/input.txt input-12.txt
+
+import sys
+from collections import defaultdict, Counter
+from itertools import *
+from functools import *
+# https://more-itertools.readthedocs.io/en/stable/
+from more_itertools import *
+# import numpy as np
+# import scipy.signal
+
+import heapq
+
+lines = [line.strip() for line in open("input-12-test.txt")]
+lines = [line.strip() for line in open("input-12.txt")]
+orig_lines = lines
+W = len(orig_lines[0])
+H = len(orig_lines)
+
+def foo(xx, yy):
+    global W, H, orig_lines
+    lines = [list(line) for line in orig_lines]
+
+    heap = []
+    weights = []
+    E = None
+
+    for y in range(H):
+        weights.append([])
+        for x in range(W):
+            w = 1000000
+            if lines[y][x] == 'S':
+                lines[y][x] = 'a'
+
+            if lines[y][x] == 'E':
+                E = (y,x)
+                lines[y][x] = 'z'
+
+            if xx == x and yy == y:
+                if lines[yy][xx] != 'a':
+                    return
+                w = 0
+
+            heapq.heappush(heap, (w, y, x))
+            weights[y].append(w)
+
+    while len(heap) > 0:
+        weight, y, x = heapq.heappop(heap)
+        #print(weight, y, x)
+        w = weights[y][x] + 1
+        if x > 0 and ord(lines[y][x - 1]) <= ord(lines[y][x]) + 1 and w < weights[y][x - 1]:
+            weights[y][x - 1] = w
+            heapq.heappush(heap, (w, y, x - 1))
+        if x < W - 1 and ord(lines[y][x + 1]) <= ord(lines[y][x]) + 1 and w < weights[y][x + 1]:
+            weights[y][x + 1] = w
+            heapq.heappush(heap, (w, y, x + 1))
+        if y > 0 and ord(lines[y - 1][x]) <= ord(lines[y][x]) + 1 and w < weights[y - 1][x]:
+            weights[y - 1][x] = w
+            heapq.heappush(heap, (w, y - 1, x))
+        if y < H - 1 and ord(lines[y + 1][x]) <= ord(lines[y][x]) + 1 and w < weights[y + 1][x]:
+            weights[y + 1][x] = w
+            heapq.heappush(heap, (w, y + 1, x))
+        if (y, x) == E:
+            return weight
+
+
+min_w = 10000
+for yy in range(H):
+    for xx in range(W):
+        w = foo(xx, yy)
+        print(xx, yy, w)
+        if w is not None and w < min_w:
+            min_w = w
+print(min_w)
