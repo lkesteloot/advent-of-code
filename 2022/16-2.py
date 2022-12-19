87e7e835b63dafc2f6f72025d620de3056d5938a
Lawrence Kesteloot <lk@teamten.com>
2022-12-18 16:45:06 -0800

    Add comments.

--- 2022/16-2.py
+++ 2022/16-2.py
@@ -11,63 +11,97 @@ class Valve:
         self.flow = flow
         self.neighbors = neighbors
 
+# Parse and find AA.
 aa = None
-vs = {}
+valves = {}
 for line in lines:
     # Valve HH has flow rate=22; tunnel leads to valve GG
     name, flow, *neighbors = re.findall(r'([A-Z][A-Z]|[0-9]+)', line)
     v = Valve(name, int(flow), neighbors)
     if name == "AA":
         aa = v
-    vs[name] = v
+    valves[name] = v
 
+# Breadth-first search to find the distance from start_name to all other
+# nodes that have non-zero flow.
 def get_dist(start_name):
     q = collections.deque([(start_name, 0)])
     d = {}
     while q:
         name, dist = q.popleft()
-        neighbors = vs[name].neighbors
+        neighbors = valves[name].neighbors
         for n in neighbors:
             if n not in d:
                 d[n] = dist + 1
                 q.append((n, dist + 1))
 
+    # Don't include ourselves.
     del d[start_name]
-    d = {n: d for n, d in d.items() if vs[n].flow > 0}
+
+    # Remove zero-flow valves.
+    d = {n: d for n, d in d.items() if valves[n].flow > 0}
 
     return d
 
 # Precompute distance to every other flowable node.
 dists = {}
-for name in vs:
+for name in valves:
     dists[name] = get_dist(name)
 
-def get_pressure(names, opened, time_left, other_left):
+# Find the pressure from this point.
+# players is 2-element array of players, each being a room name.
+#    The function will move players[0].
+# opened is a set of players of opened valves.
+# time_left is the number of minutes left.
+# other_left is the number of minutes that players[1] has left
+#    in their current step. This is 0 if they are available
+#    to move right away.
+def get_pressure(players, opened, time_left, other_left):
+    # Can't do anything in this little time, since you'd
+    # have to move somewhere and open a valve.
     if time_left <= 2:
         return 0
 
-    # Working on the first person.
-    name = names[0]
+    # Working on the first player.
+    player = players[0]
 
+    # The best pressure we can find.
     pressure = 0
 
-    for new_name, dist in dists[name].items():
-        if new_name not in opened and dist + 1 < time_left:
-            opened.add(new_name)
-            new_pressure = (time_left - dist - 1)*vs[new_name].flow
+    # Try all possible valves we can reach from here.
+    for next_room, dist in dists[player].items():
+        # Only go to open valves, and only to valves we can reach in time.
+        if next_room not in opened and dist + 1 < time_left:
+            # Open the valve.
+            opened.add(next_room)
+            # Add this valve's pressure for the remainder of the time.
+            new_pressure = (time_left - dist - 1)*valves[next_room].flow
+            # And decrease time for the distance and the opening of the valve.
             new_time_left = time_left - dist - 1
+            # See how much time players[1] has left on their move *after* our move.
             new_other_left = other_left - dist - 1
             if new_other_left >= 0:
-                new_names = [new_name, names[1]]
+                # players[1] has time left, so we recurse normally and get players[0] to
+                # move again.
+                new_names = [next_room, players[1]]
             else:
-                new_names = [names[1], new_name]
+                # players[1] will run out of time on their move before our move finishes,
+                # so we have to move them first. Swap the players so that the cursion
+                # moves players[1].
+                new_names = [players[1], next_room]
+                # Compute how much time will be left for players[0], since we're not
+                # doing their entire move right now.
                 new_other_left = -new_other_left
+                # We decreased the time left too much and encroached into the other
+                # player's time. Fix that.
                 new_time_left += new_other_left
 
             p = new_pressure + get_pressure(new_names, opened, new_time_left, new_other_left)
             if p > pressure:
                 pressure = p
-            opened.remove(new_name)
+
+            # Undo opening the valve.
+            opened.remove(next_room)
 
     return pressure
 
