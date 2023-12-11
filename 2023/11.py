3cf25b8110c056914dce054f5c21d93332acafda
Lawrence Kesteloot <lk@teamten.com>
2023-12-10 23:04:36 -0800

    Use combinations.

--- 2023/11.py
+++ 2023/11.py
@@ -1,4 +1,6 @@
 
+from itertools import combinations
+
 lines = open("input-11.txt").read().splitlines()
 
 expand = 1000000
@@ -17,8 +19,6 @@ def adjust_p(p):
 
 galaxies = [adjust_p(p) for p in galaxies]
 
-total = sum(abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1])
-    for i in range(len(galaxies) - 1) for j in range(i + 1, len(galaxies)))
+total = sum(abs(g1[0] - g2[0]) + abs(g1[1] - g2[1]) for g1, g2 in combinations(galaxies, 2))
 
 print(total)
-
