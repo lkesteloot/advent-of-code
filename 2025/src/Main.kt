e95693047dbf678f53c9de4f62e978f8ea09a8d5
Lawrence Kesteloot <lk@teamten.com>
2025-12-08 14:30:54 -0800

    Day 8.

--- 2025/src/Main.kt
+++ 2025/src/Main.kt
@@ -2,13 +2,15 @@ import java.io.File
 import kotlin.math.max
 import kotlin.time.measureTimedValue
 
+enum class Part { ONE, TWO }
+
 fun myAssert(assertion: Boolean) {
     if (!assertion) {
         throw AssertionError()
     }
 }
 
-fun day1(lines: List<String>, part: Int): Long {
+fun day1(lines: List<String>, part: Part): Long {
     var pos = 50
     var count = 0L
     for (line in lines) {
@@ -16,16 +18,20 @@ fun day1(lines: List<String>, part: Int): Long {
         val distance = line.drop(1).toInt()
         val step = if (direction == 'R') 1 else -1
 
-        if (part == 1) {
-            pos = Math.floorMod(pos + step*distance, 100)
-            if (pos == 0) {
-                count += 1
-            }
-        } else {
-            repeat (distance) {
-                pos = Math.floorMod(pos + step, 100)
+        when (part) {
+            Part.ONE -> {
+                pos = Math.floorMod(pos + step*distance, 100)
                 if (pos == 0) {
                     count += 1
+                }
+            }
+
+            Part.TWO -> {
+                repeat (distance) {
+                    pos = Math.floorMod(pos + step, 100)
+                    if (pos == 0) {
+                        count += 1
+                    }
                 }
             }
         }
@@ -34,23 +40,23 @@ fun day1(lines: List<String>, part: Int): Long {
     return count
 }
 
-fun day2(lines: List<String>, part: Int): Long {
+fun day2(lines: List<String>, part: Part): Long {
     var result = 0L
 
     val invalidIds = mutableSetOf<Long>()
 
     fun tryRange(first: String, last: String) {
         assert(first.length == last.length)
-        if (part == 1 && first.length % 2 != 0) {
+        if (part == Part.ONE && first.length % 2 != 0) {
             return
         }
         val firstValue = first.toLong()
         val lastValue = last.toLong()
         val halfLength = last.length / 2
-        val lenRange = (if (part == 1) halfLength else 1)..halfLength
+        val lenRange = (if (part == Part.ONE) halfLength else 1)..halfLength
         for (len in lenRange) {
             var counter = first.take(len).toLong()
-            val repeat = if (part == 1) 2 else first.length / len
+            val repeat = if (part == Part.ONE) 2 else first.length / len
             while (true) {
                 val id = counter.toString().repeat(repeat).toLong()
                 if (id > lastValue) {
@@ -80,9 +86,9 @@ fun day2(lines: List<String>, part: Int): Long {
     return result
 }
 
-fun day3(lines: List<String>, part: Int): Long {
+fun day3(lines: List<String>, part: Part): Long {
     var result = 0L
-    val batteryCount = if (part == 1) 2 else 12
+    val batteryCount = if (part == Part.ONE) 2 else 12
 
     for (line in lines) {
         var joltage = 0L
@@ -102,7 +108,7 @@ fun day3(lines: List<String>, part: Int): Long {
     return result
 }
 
-fun day4(lines: List<String>, part: Int): Long {
+fun day4(lines: List<String>, part: Part): Long {
     var result = 0L
 
     val rows = lines.size
@@ -146,8 +152,8 @@ fun day4(lines: List<String>, part: Int): Long {
     }
 
     when (part) {
-        1 -> result = accessiblePositions.size.toLong()
-        2 -> {
+        Part.ONE -> result = accessiblePositions.size.toLong()
+        Part.TWO -> {
             while (!accessiblePositions.isEmpty()) {
                 val toProcess = accessiblePositions.toSet()
                 accessiblePositions.clear()
@@ -169,8 +175,8 @@ fun day4(lines: List<String>, part: Int): Long {
     return result
 }
 
-fun day5(lines: List<String>, part: Int): Long {
-    var result = 0L
+fun day5(lines: List<String>, part: Part): Long {
+    var result: Long
 
     // Load ranges.
     val blankIndex = lines.indexOf("")
@@ -195,7 +201,7 @@ fun day5(lines: List<String>, part: Int): Long {
     }
 
     when (part) {
-        1 -> {
+        Part.ONE -> {
             result = lines.drop(blankIndex + 1)
                 .map { it.toLong() }
                 .count { id ->
@@ -211,17 +217,17 @@ fun day5(lines: List<String>, part: Int): Long {
                 .toLong()
         }
 
-        2 -> result = ranges.sumOf { it.last - it.first + 1 }
+        Part.TWO -> result = ranges.sumOf { it.last - it.first + 1 }
     }
 
     return result
 }
 
-fun day6(lines: List<String>, part: Int): Long {
+fun day6(lines: List<String>, part: Part): Long {
     var result = 0L
 
     when (part) {
-        1 -> {
+        Part.ONE -> {
             val spaces = " +".toRegex()
             val numbers = lines.take(lines.size - 1).map { it.trim().split(spaces).map { it.toLong() } }
             val ops = lines[lines.size - 1].trim().split(spaces)
@@ -236,7 +242,7 @@ fun day6(lines: List<String>, part: Int): Long {
             }
         }
 
-        2 -> {
+        Part.TWO -> {
             val columnCount = lines[0].length
             myAssert(lines.all { it.length == columnCount })
             val rowCount = lines.size - 1
@@ -264,7 +270,7 @@ fun day6(lines: List<String>, part: Int): Long {
     return result
 }
 
-fun day7(lines: List<String>, part: Int): Long {
+fun day7(lines: List<String>, part: Part): Long {
     val width = lines[0].length
 
     var tach = List(width) { index -> if (lines[0][index] == 'S') 1L else 0L }
@@ -291,19 +297,108 @@ fun day7(lines: List<String>, part: Int): Long {
     }
 
     return when (part) {
-        1 -> splitCount
-        2 -> tach.sum()
-        else -> throw Error()
+        Part.ONE -> splitCount
+        Part.TWO -> tach.sum()
+    }
+}
+
+fun day8(lines: List<String>, part: Part): Long {
+    data class Box(val x: Long, val y: Long, val z: Long, var parent: Box?, var size: Int)
+    data class Pair(val b1: Box, val b2: Box, val dist: Long)
+
+    val boxes = lines.map { line ->
+        val (x, y, z) = line.split(',').map(String::toLong)
+        Box(x, y, z, null, 1)
+    }
+
+    // https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    fun rootOf(box: Box): Box {
+        var root = box
+        while (true) {
+            val parent = root.parent ?: break
+            root = parent
+        }
+
+        var walk = box
+        while (true) {
+            val parent = walk.parent ?: break
+            walk.parent = root
+            walk = parent
+        }
+
+        return root
+    }
+
+    fun merge(b1: Box, b2: Box) {
+        // Find roots.
+        var x = rootOf(b1)
+        var y = rootOf(b2)
+        if (x == y) {
+            // x and y are already in the same set.
+            return
+        }
+
+        // If necessary, swap variables to ensure that
+        // x has at least as many descendants as y.
+        if (x.size < y.size) {
+            val tmp = x
+            x = y
+            y = tmp
+        }
+
+        y.parent = x
+        x.size += y.size
+    }
+
+    val pairs = mutableListOf<Pair>()
+    for (i in 0 until boxes.size - 1) {
+        val bi = boxes[i]
+        for (j in i + 1 until boxes.size) {
+            val bj = boxes[j]
+            val dx = bi.x - bj.x
+            val dy = bi.y - bj.y
+            val dz = bi.z - bj.z
+            pairs.add(Pair(bi, bj, dx*dx + dy*dy + dz*dz))
+        }
+    }
+    pairs.sortBy { pair -> pair.dist }
+
+    when (part) {
+        Part.ONE -> {
+            val connectionCount = if (boxes.size < 100) 10 else 1000
+            pairs.take(connectionCount).forEach { pair ->
+                merge(pair.b1, pair.b2)
+            }
+
+            return boxes
+                .map(::rootOf)
+                .toSet()
+                .sortedBy { box -> -box.size }
+                .take(3)
+                .fold(1) { product, box -> product*box.size }
+                .toLong()
+        }
+
+        Part.TWO -> {
+            pairs.forEach { pair ->
+                merge(pair.b1, pair.b2)
+                if (rootOf(pair.b1).size == boxes.size) {
+                    return pair.b1.x*pair.b2.x
+                }
+            }
+            val pair = pairs.last()
+            return pair.b1.x*pair.b2.x
+        }
     }
 }
 
 fun main() {
     val testDay = -1 // or -1 to disable
-    arrayOf(::day1, ::day2, ::day3, ::day4, ::day5, ::day6, ::day7).forEachIndexed { index, dayFunction ->
+    arrayOf(::day1, ::day2, ::day3, ::day4, ::day5, ::day6, ::day7, ::day8).forEachIndexed { index, dayFunction ->
         val day = index + 1
         val filename = if (day == testDay) "day$day-test.txt" else "day$day.txt"
         val lines = File(filename).readLines()
-        for (part in 1..2) {
+        for (part in Part.entries) {
             val (result, timeTaken) = measureTimedValue {
                 dayFunction(lines, part)
             }
